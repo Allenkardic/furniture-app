2fc14e0074bddc404c8b11306d7d279bebf8c3d1
Aliakwe <aliakwe@yahoo.com>
2020-01-08 23:47:10 +0100

    Updates

---
+++ precache-manifest.8895c16dc120e353dc94e5b3b405cb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fba61fc057cc0cde58858ca6990b07a",
+    "url": "/furniture-app/index.html"
+  },
+  {
+    "revision": "74736184fa8a52c333ff",
+    "url": "/furniture-app/static/css/main.6d095c92.chunk.css"
+  },
+  {
+    "revision": "cb678271274346c0d222",
+    "url": "/furniture-app/static/js/2.d8d70d24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/furniture-app/static/js/2.d8d70d24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74736184fa8a52c333ff",
+    "url": "/furniture-app/static/js/main.b4ac88a4.chunk.js"
+  },
+  {
+    "revision": "2dc1880df4e2d4df672d",
+    "url": "/furniture-app/static/js/runtime-main.d684516e.js"
+  },
+  {
+    "revision": "559fb067a5bfb5c335bfeab2056a4b55",
+    "url": "/furniture-app/static/media/sofa1.559fb067.png"
+  }
+]);
